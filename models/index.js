39fdee58f25cc0fa219cf1e21a0a928e9dfe4073
Joyce Ren <joyce.y.ren@gmail.com>
2017-10-08 11:44:43 -0400

    complete

--- models/index.js
+++ models/index.js
@@ -8,9 +8,37 @@ const Tea = db.define('tea', {
   title: Sequelize.STRING,
   description: Sequelize.TEXT,
   price: Sequelize.INTEGER,
-  category: Sequelize.ENUM('green', 'black', 'herbal')
+  category: Sequelize.ENUM('green', 'black', 'herbal'),
+  dollarPrice: {
+    type: Sequelize.VIRTUAL,
+    get() {return `$${(this.price/100).toFixed(2)}`}
+  }
 }, {
-  // add more functionality to our Tea model here!
+  hooks: {
+    beforeCreate: (tea) => {
+      tea.title = tea.title.split(' ').map(e => e[0].toUpperCase()+e.slice(1)).join(' ')
+    }
+  },
+  instanceMethods: {
+    findSimilar: function() {console.log(this)}
+  }
 })
+
+Tea.findByCategory = function(cat){
+  return this.findAll({
+    where: {
+      category: cat
+    }
+  })
+}
+
+Tea.prototype.findSimilar = function(){
+  return Tea.findAll({
+    where: {
+      title: this.title
+    }
+  })
+}
+
 
 module.exports = { db, Tea };
